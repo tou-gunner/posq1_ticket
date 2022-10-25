1987a4d5f42d6f9764d39572268e50c3512829d0
sengdavong <tou.sengdavong@gmail.com>
2022-10-25 12:54:34 +0700

    initialCreate

---
+++ android/app/src/main/kotlin/com/seng/q1ticket/posq1_ticket/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.seng.q1ticket.posq1_ticket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,49 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<blue_thermal_printer/BlueThermalPrinterPlugin.h>)
+#import <blue_thermal_printer/BlueThermalPrinterPlugin.h>
+#else
+@import blue_thermal_printer;
+#endif
+
+#if __has_include(<charset_converter/CharsetConverterPlugin.h>)
+#import <charset_converter/CharsetConverterPlugin.h>
+#else
+@import charset_converter;
+#endif
+
+#if __has_include(<flutter_bluetooth_basic/FlutterBluetoothBasicPlugin.h>)
+#import <flutter_bluetooth_basic/FlutterBluetoothBasicPlugin.h>
+#else
+@import flutter_bluetooth_basic;
+#endif
+
+#if __has_include(<path_provider_ios/FLTPathProviderPlugin.h>)
+#import <path_provider_ios/FLTPathProviderPlugin.h>
+#else
+@import path_provider_ios;
+#endif
+
+#if __has_include(<permission_handler_apple/PermissionHandlerPlugin.h>)
+#import <permission_handler_apple/PermissionHandlerPlugin.h>
+#else
+@import permission_handler_apple;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [BlueThermalPrinterPlugin registerWithRegistrar:[registry registrarForPlugin:@"BlueThermalPrinterPlugin"]];
+  [CharsetConverterPlugin registerWithRegistrar:[registry registrarForPlugin:@"CharsetConverterPlugin"]];
+  [FlutterBluetoothBasicPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBluetoothBasicPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
+}
+
+@end
